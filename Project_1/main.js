6572c4c89a0812312ff7d3bba8419cacfb5f30ac
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-05-12 20:37:18 +0300

    Equality

--- Project_1/main.js
+++ Project_1/main.js
@@ -1,16 +1,4 @@
-//assagment operator
-let x = 10
-let y = 5
-console.log(x++) //10
-
-//boolen
-console.log(x === y)
-console.log(x > y)
-
-//logical
-// || or
-const isValidNumber = x > 8 && 8 > y
-console.log(isValidNumber)
-
-const isEven = 10 % 2 === 1 ? true : false
-console.log(isEven)
+const var1 = 10
+const var2 = "10"
+console.log(var1 === var2) //false
+console.log(var1 == var2) //true
