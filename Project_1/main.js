fafe4cf80ee2c579e668c22bfce7a37c2ea3047f
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-05-12 20:12:07 +0300

    Data types

---
+++ Project_1/main.js
@@ -0,0 +1,24 @@
+console.log("hello ilayda")
+
+//variables
+
+let age = 25
+console.log(age)
+
+const salary = 8000
+console.log(salary)
+
+//property of object
+const person = {
+    "firstName": "ilayda",
+    'age': "25"
+}
+console.log(person.firstName)
+
+const oddNumbers = [1, 3, 5, 7, 9]
+console.log(oddNumbers[0])
+
+let a = 10
+a = 'string'
+a = true
+console.log(a)
