c3c67112618abad716babf0e3047a762b3c07249
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-05-13 14:47:29 +0300

    Binding

--- Project_1/main.js
+++ Project_1/main.js
@@ -10,3 +10,13 @@ function sayMyName() {
     console.log(`My name is ${this.name}`)
 }
 sayMyName.call(person)
+
+function Person(name) {
+    //this={}
+    this.name = name
+}
+const p1 = new Person("peter")
+const p2 = new Person("white")
+
+console.log(p1.name, p2.name)
+
